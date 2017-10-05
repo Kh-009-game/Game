467c2dcaea4ec51d36ac4be4840c9f071dafad28
cyrstron <kirstronko@gmail.com>
2017-10-05 22:41:55 +0300

    locker service

--- services/locker-service.js
+++ services/locker-service.js
@@ -3,25 +3,72 @@ class Locker {
 		this.lockedKeys = [];
 	}
 
+	findKey(key) {
+		if (typeof key === 'object') {
+			return this.findCompositeKey(key);
+		}
+
+		return this.findPrimitiveKey(key);
+	}
+
+	findPrimitiveKey(key) {
+		return this.lockedKeys.indexOf(key);
+	}
+
+	findCompositeKey(key) {
+		const lockedKeys = this.lockedKeys;
+
+		for (
+			let lockArrIndex = 0, lockArrMaxIndex = lockedKeys.length;
+			lockArrIndex < lockArrMaxIndex;
+			lockArrIndex += 1
+		) {
+			const lockedKey = lockedKeys[lockArrIndex];
+			if (this.checkPropsEquality(lockedKey, key)) {
+				return lockArrIndex;
+			}
+		}
+
+		return -1;
+	}
+
+	checkPropsEquality(obj, obj2) {
+		const keys = Object.keys(obj);
+		for (
+			let keysIndex = 0, keysMaxIndex = keys.length;
+			keysIndex < keysMaxIndex;
+			keysIndex += 1
+		) {
+			const key = keys[keysIndex];
+
+			if (typeof obj[key] === 'object') {
+				if (!this.checkPropsEquality(obj[key], obj2[key])) {
+					return false;
+				}
+			}
+
+			if (obj[key] !== obj2[key]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	checkKey(key) {
-		const keyIndex = this.lockedKeys.indexOf(key);
+		const keyIndex = this.findKey(key);
+
 		return keyIndex !== -1;
 	}
 
-	checkCompositeKey(key) {
-		const keyKeys = Object.keys(key);
+	addKey(key) {
+		this.lockedKeys.push(key);
+	}
 
-		for (let i = 0, j = keyKeys.length; i < j; i += 1) {
-			// if (typeof key[keyKeys[i]] === 'Object') {
-			// 	if (this.checkCompositeKey(key[keyKeys[i]])) {
-			// 		return true;
-			// 	}
-			// }
-			for (let k = 0, l = this.lockedKeys.length; i < l; k += 1) {
-				if (key[keyKeys[i]] === this.lockedKeys[k][keyKeys[i]]) {
-					return true;
-				}
-			}
-		}
+	deleteKey(key) {
+		const keyIndex = this.findKey(key);
+
+		this.lockedKeys.splice(keyIndex, 1);
 	}
 }
+
+module.exports = Locker;
