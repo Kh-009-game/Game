aa35fb0c9268d86546e14ac062e9d835c9498ca7
cyrstron <kirstronko@gmail.com>
2017-10-13 09:40:28 +0300

    contains method

--- services/fieldBounds-service.js
+++ services/fieldBounds-service.js
@@ -168,33 +168,104 @@ class BoundService {
 		return nextLoc;
 	}
 
-	containsLocByNorthWest(point) {
-		const borderLocsOnLat = [];
+	get contourPoly() {
+		return this.extractContour;
+	}
 
-		this.borderLocs.forEach((location) => {
-			const borderLat = location.northWest.lat;
-			if (borderLat === point.lat) {
-				borderLocsOnLat.push(location);
-			}
-		});
+	extractContour() {
 
-		const len = borderLocsOnLat.length;
+	}
+
+	// containsLocByNorthWest(point) {
+	// 	const borderLocsOnLat = [];
+
+	// 	this.borderLocs.forEach((location) => {
+	// 		const borderLat = location.northWest.lat;
+	// 		if (borderLat === point.lat) {
+	// 			borderLocsOnLat.push(location);
+	// 		}
+	// 	});
+
+	// 	const len = borderLocsOnLat.length;
+
+	// 	if (!len) return false;
+
+	// 	for (let i = 0; i < len; i += 2) {
+	// 		const centerLng1 = borderLocsOnLat[i].northWest.lng;
+	// 		const centerLng2 = borderLocsOnLat[i + 1].northWest.lng;
+
+	// 		const lngMax = centerLng1 > centerLng2 ? centerLng1 : centerLng2;
+	// 		const lngMin = centerLng1 < centerLng2 ? centerLng1 : centerLng2;
+
+	// 		if ((point.lng < lngMax) && (point.lng > lngMin)) {
+	// 			return true;
+	// 		}
+	// 	}
+
+	// 	return false;
+	// }
+
+	containsLocByNorthWest(northWest, inclusive) {
+		const location = EmptyLocation.createLocationByPoint(northWest);
+		const polygon = location.mapFeatureCoords;
+
+		if (inclusive) {
+			return this.containsInclusive(polygon);
+		}
+
+		return this.containsExclusive(polygon);
+	}
+
+	containsInclusive(polygonPoints) {
+		for (let i = 0, len = polygonPoints.length; i < len; i += 1) {
+			if (this.containsPoint(polygonPoints[i])) return true;
+		}
+		return false;
+	}
+
+	containsExclusive(polygonPoints) {
+		for (let i = 0, len = polygonPoints.length; i < len; i += 1) {
+			if (!this.containsPoint(polygonPoints[i])) return false;
+		}
+		return true;
+	}
+
+	containsPoint(point) {
+		const intersectPoints = this.getIntersectionByLat(point.lat);
+
+		const len = intersectPoints.length;
 
 		if (!len) return false;
 
 		for (let i = 0; i < len; i += 2) {
-			const centerLng1 = borderLocsOnLat[i].northWest.lng;
-			const centerLng2 = borderLocsOnLat[i + 1].northWest.lng;
-
-			const lngMax = centerLng1 > centerLng2 ? centerLng1 : centerLng2;
-			const lngMin = centerLng1 < centerLng2 ? centerLng1 : centerLng2;
-
-			if ((point.lng < lngMax) && (point.lng > lngMin)) {
+			if ((point.lng > intersectPoints[i].lng) && (point.lng < intersectPoints[i + 1].lng)) {
 				return true;
 			}
 		}
 
 		return false;
+	}
+
+	getIntersectionByLat(lat) {
+		const intersectPoints = [];
+
+		this.borderEquations.forEach((eq, i) => {
+			const intersectLng = eq.getLng(lat);
+
+			const pointA = this.rawPoints[i];
+			const pointB = this.rawPoints[i + 1] || this.rawPoints[0];
+			const maxLng = pointA.lng > pointB.lng ? pointA.lng : pointB.lng;
+			const minLng = pointA.lng < pointB.lng ? pointA.lng : pointB.lng;
+
+			if ((intersectLng < maxLng) && (intersectLng > minLng)) {
+				intersectPoints.push({
+					lat,
+					lng: intersectLng
+				});
+			}
+		});
+
+		return intersectPoints.sort((pointA, pointB) => (pointA.lng - pointB.lng));
 	}
 }
 
